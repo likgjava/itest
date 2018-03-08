a15b73ad52fbf6014afa2763a8aced2fb4a491f0
likaige <likaige@100tal.com>
2018-03-08 21:12:47 +0800

    Initial commit

---
+++ ams/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def hello(request):
+    list = ["HTML", "CSS", "jQuery", "Python", "Django"]
+    info_dict = {'site': u'自强学堂', 'content': u'各种IT技术教程'}
+    data = {'name': '张三', 'list': list, 'info_dict': info_dict}
+
+    return render(request, 'index.html', data)
+
+
+def add(request):
+    a = request.GET['a']
+    b = request.GET['b']
+    c = int(a) + int(b)
+    return HttpResponse(str(c))
+
+
+def add2(request, a, b):
+    c = a + b
+    return HttpResponse(str(c))
